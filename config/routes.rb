6b4b23ba388523c7a85ced02dc1f97d622c4481b
Jeremy Neal <hello@radglob.com>
2022-08-07 17:04:35 -0400

    Change some user stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'auth', path_names: { 'sign_in': 'login', 'sign_out': 'logout', registration: 'register' }
+  devise_for :users
   root 'home#index'
 end
