0008ecc8cda4fc0af79f8bac9f4d5acb22d442b0
Jeremy Neal <hello@radglob.com>
2022-09-27 21:41:30 -0400

    More wip mobile design, colors and such.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        serif: ['Garamond', 'Georgia', 'ui-serif'],
       },
       backgroundImage: {
         'hero-pattern': "linear-gradient(to bottom, rgb(104,47,74,0.75), rgb(104,47,74,0)), url('/img/water-tower-hero-wide.jpg')"
@@ -19,11 +20,12 @@ module.exports = {
         'mobile-offset': '60% 33%'
       },
       colors: {
-        'maroon': {
-          dark: 'rgb(104,47,74,0.75)',
-          transparent: 'rgb(104,47,74,0)'
-        },
-        'gold': '#fec600'
+        olive: '#354A18',
+        cream: '#F9EBF3',
+        'forest-green': {
+          light: '#00A385',
+          dark: '#00493A'
+        }
       }
     },
   },
