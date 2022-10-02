3883073a06b5f62f090772a1c35c887ca2211569
Jeremy Neal <hello@radglob.com>
2022-10-02 15:14:02 -0400

    Calculate correct days and show which are outside of the month.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,3 +1,8 @@
 class CalendarController < ApplicationController
   include CalendarHelper
+
+  def index
+    @date = Date.today
+    @weeks = weeks_for_calendar(@date)
+  end
 end
--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -17,4 +17,17 @@ module CalendarHelper
       'Sat'
     end
   end
+
+  # Generates a range of dates from the first day of the first week of the month (which may be before the
+  # start of the month) until the last day of the last week of the month (which also may not be within the
+  # month.
+  def weeks_for_calendar(date)
+    first_day_of_month = date.at_beginning_of_month
+    offset = first_day_of_month.wday.days
+    first_day_of_first_week = first_day_of_month - offset
+    last_day_of_month = date.at_end_of_month
+    offset = (last_day_of_month.wday - 6).days
+    last_day_of_last_week = last_day_of_month - offset
+    (first_day_of_first_week..last_day_of_last_week).each_slice(7).to_a
+  end
 end
