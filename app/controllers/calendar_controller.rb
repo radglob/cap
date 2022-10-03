a1874f002f987485df7f65e195dcc3c49c36805d
Jeremy Neal <hello@radglob.com>
2022-10-02 21:17:53 -0400

    Mobile layout for calendar based on fastmail's mobile view. May need something separate for larger form factors like ipads.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,8 +1,28 @@
 class CalendarController < ApplicationController
   include CalendarHelper
 
+  require 'ostruct'
+
   def index
     @date = Date.today
     @weeks = weeks_for_calendar(@date)
+    @events = events_for_month(@date)
+    request.variant = determine_variant
+  end
+
+  private
+
+  def events_for_month(_)
+    []
+  end
+
+  def determine_variant
+    variant = nil
+    variant = :mobile if mobile?
+    variant
+  end
+
+  def mobile?
+    request.user_agent =~ /Mobile|webOS/
   end
 end
