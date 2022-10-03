a966d2c1f4164d4c6532e0ce4e37144a8060f75b
Jeremy Neal <hello@radglob.com>
2022-10-02 21:22:47 -0400

    Simplify day_of_week function with Date constants.

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,21 +1,6 @@
 module CalendarHelper
-  def day_of_week(i)
-    case i
-    when 0
-      'Sun'
-    when 1
-      'Mon'
-    when 2
-      'Tues'
-    when 3
-      'Wed'
-    when 4
-      'Thu'
-    when 5
-      'Fri'
-    when 6
-      'Sat'
-    end
+  def day_of_week(wday)
+    Date::DAYNAMES[wday]
   end
 
   # Generates a range of dates from the first day of the first week of the month (which may be before the
